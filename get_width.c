c85a93f4999d8df9bdb5326849619451fc3a14e2
Piedoskie <111045578+Piedoskie@users.noreply.github.com>
2024-01-31 00:02:58 +0200

    Update get_width.c

--- get_width.c
+++ get_width.c
@@ -1,37 +1,36 @@
 #include "main.h"
 
 /**
- * printf_octal - prints a binary number
- * @num: number of arguements
- * @printed: the printed characters
- * Return: printed charcaters
+ * get_width - Calculates the width for printing
+ * @format: Formatted string in which to print the arguments.
+ * @i: List of arguments to be printed.
+ * @list: list of arguments.
+ *
+ * Return: width.
  */
-
-int printf_octal(unsigned int num, int printed)
+int get_width(const char *format, int *i, va_list list)
 {
-	int oct[100], i = 0, j;
+	int curr_i;
+	int width = 0;
 
-	while (num != 0)
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		int remainder = num % 8;
-
-		oct[i] = 48 + remainder;
-		i++;
-		num /= 8;
-	}
-
-	if (i == 0)
-	{
-		_putchar('0');
-		printed++;
-	}
-	else
-	{
-		for (j = i - 1; j >= 0; j--)
+		if (is_digit(format[curr_i]))
 		{
-			_putchar(oct[j]);
-			printed++;
+			width *= 10;
+			width += format[curr_i] - '0';
 		}
+		else if (format[curr_i] == '*')
+		{
+			curr_i++;
+			width = va_arg(list, int);
+			break;
+		}
+		else
+			break;
 	}
-	return (printed);
+
+	*i = curr_i - 1;
+
+	return (width);
 }
